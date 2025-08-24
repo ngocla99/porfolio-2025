28fc8782b18963a7c044a49c01a122e56c5e8bc6
ngocla99 <ngoc.leanh1999@gmail.com>
2025-08-24 11:04:11 +0700

    feat: add opportunities section to profile

---
+++ src/app/(porfolio)/_components/opportunities-section.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { ButtonLink } from "@/components/ui/button-link";
+import { cn } from "@/lib/utils";
+
+export function OpportunitiesSection() {
+  const [emailHovered, setEmailHovered] = React.useState(false);
+  const [copyHovered, setCopyHovered] = React.useState(false);
+
+  const getTextHover = () => {
+    if (emailHovered) {
+      return "Open your email app.";
+    }
+
+    if (copyHovered) {
+      return "Copy address to your clipboard.";
+    }
+
+    return "Send me an email.";
+  };
+
+  return (
+    <div className="mt-8">
+      <p className="text-balance flex items-center gap-2.5 font-proto text-xs text-warning sm:gap-2 sm:text-sm">
+        <span className="mb-[0.1em] h-1.5 w-1.5 shrink-0 rounded-full bg-warning"></span>
+        <span>Open to exciting opportunities globally.</span>
+      </p>
+      <div className="flex items-center gap-3 mt-4">
+        <div className="flex gap-3">
+          <a
+            href="mailto:ngoc.leanh1999@gmail.com"
+            className="flex items-center justify-center w-10 h-10 rounded-xl ring-1 ring-border hover:ring-2 hover:ring-accent active:scale-95 text-accent outline-none focus-visible:ring-2 focus-visible:ring-accent"
+            onMouseEnter={() => setEmailHovered(true)}
+            onMouseLeave={() => setEmailHovered(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+              <polyline points="22,6 12,13 2,6"></polyline>
+            </svg>
+          </a>
+          <button
+            className="flex items-center justify-center w-10 h-10 rounded-xl ring-1 ring-border hover:ring-2 hover:ring-accent active:scale-95 text-accent outline-none focus-visible:ring-2 focus-visible:ring-accent"
+            onMouseEnter={() => setCopyHovered(true)}
+            onMouseLeave={() => setCopyHovered(false)}
+          >
+            <svg
+              className="absolute"
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              style={{ opacity: 1, transform: "none" }}
+            >
+              <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+              <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+            </svg>
+          </button>
+        </div>
+        <p className="pl-0.5 body-sm text-accent">
+          Want to discuss opportunities? <br />
+          <span
+            className={cn(
+              "block",
+              (emailHovered || copyHovered) && "text-white"
+            )}
+          >
+            {getTextHover()}
+          </span>
+        </p>
+      </div>
+      <p className="text-balance flex items-center gap-2.5 pt-5 font-proto text-xs text-body sm:gap-2 sm:text-sm">
+        or you can{" "}
+        <ButtonLink className="text-friends text-sm" href="/projects">
+          see my work
+        </ButtonLink>
+      </p>
+    </div>
+  );
+}
--- src/app/(porfolio)/_components/profile-section.tsx
+++ src/app/(porfolio)/_components/profile-section.tsx
@@ -8,6 +8,7 @@ import { TimelineWork } from "./time-line-work";
 import React from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { OpportunitiesSection } from "./opportunities-section";
 
 export function ProfileSection() {
   // FIXME: This is a hack to prevent white flash when the user navigates to the projects page
@@ -56,63 +57,7 @@ export function ProfileSection() {
           expectations. ✨
         </p>
       </div>
-      <div className='mt-8'>
-        <p className='text-balance flex items-center gap-2.5 font-proto text-xs text-warning sm:gap-2 sm:text-sm'>
-          <span className='mb-[0.1em] h-1.5 w-1.5 shrink-0 rounded-full bg-warning'></span>
-          <span>Open to exciting opportunities globally.</span>
-        </p>
-        <div className='flex items-center gap-3 mt-4'>
-          <div className='flex gap-3'>
-            <a
-              href='mailto:your-email@example.com'
-              className='flex items-center justify-center w-10 h-10 rounded-xl ring-1 ring-border hover:ring-2 hover:ring-accent active:scale-95 text-accent outline-none focus-visible:ring-2 focus-visible:ring-accent'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='18'
-                height='18'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path d='M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z'></path>
-                <polyline points='22,6 12,13 2,6'></polyline>
-              </svg>
-            </a>
-            <button className='flex items-center justify-center w-10 h-10 rounded-xl ring-1 ring-border hover:ring-2 hover:ring-accent active:scale-95 text-accent outline-none focus-visible:ring-2 focus-visible:ring-accent'>
-              <svg
-                className='absolute'
-                xmlns='http://www.w3.org/2000/svg'
-                width='18'
-                height='18'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                style={{ opacity: 1, transform: "none" }}
-              >
-                <rect x='9' y='9' width='13' height='13' rx='2' ry='2'></rect>
-                <path d='M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1'></path>
-              </svg>
-            </button>
-          </div>
-          <p className='pl-0.5 body-sm'>
-            Want to discuss opportunities? <br />
-            <span className='block'>Send me an email.</span>
-          </p>
-        </div>
-        <p className='text-balance flex items-center gap-2.5 pt-5 font-proto text-xs text-body sm:gap-2 sm:text-sm'>
-          or you can{" "}
-          <ButtonLink className='text-friends text-sm' href='/projects'>
-            see my work
-          </ButtonLink>
-        </p>
-      </div>
+      <OpportunitiesSection />
       <TimelineWork className='mt-14 w-full bio-text-shadow' onNavigate={handleNavigation} />
       <h2 className='custom-h2'>Elsewhere</h2>
       <div className='grid grid-cols-2 gap-8 md:flex md:flex-row md:flex-wrap md:gap-8'>
